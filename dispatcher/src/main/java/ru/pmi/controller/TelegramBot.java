b4177ad8bb33b4d82db1ed0f90213fa1b4f6a4e1
Ivan Dolgopolov <fertexop@yandex.ru>
2026-03-10 15:16:00 +0700

    feat(TelegramBot): Добавил логирование и возврат сообщения в чат клиента

--- dispatcher/src/main/java/ru/pmi/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/pmi/controller/TelegramBot.java
@@ -1,21 +1,52 @@
 package ru.pmi.controller;
 
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Component
+@Log4j
 public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${bot.token}")
+    private String botToken;
+
     @Override
     public String getBotToken() {
-        return "";
+        return botToken;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+        log.debug(originalMessage.getText());
 
+        var response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hello from bot");
+        sendAnswerMessage(response);
     }
 
     @Override
     public String getBotUsername() {
-        return "";
+        return botName;
     }
+
+    // Возврат сообщения в часть телеграмма
+    public void sendAnswerMessage(SendMessage message){
+        if (message != null){
+            try{
+                execute(message);
+            } catch (TelegramApiException e){
+                log.error(e);
+            }
+        }
+    }
+
 }
