d62962cc599076ad191ad6b4ce65d7412d4e9ac7
Ivan Dolgopolov <fertexop@yandex.ru>
2026-03-14 14:01:13 +0700

    feat/ Реализовал базовую валидацию сообщений, контроллер и сервис для интеграции RabbitMQ

---
+++ common-rabbitmq/src/main/java/ru/pmi/model/RabbitQueue.java
@@ -0,0 +1,8 @@
+package ru.pmi.model;
+
+public class RabbitQueue {
+    public static final String DOC_MESSAGE_UPDATE = "doc_message_update";
+    public static final String PHOTO_MESSAGE_UPDATE = "photo_message_update";
+    public static final String TEXT_MESSAGE_UPDATE = "test_message_update";
+    public static final String ANSWER_MESSAGE = "answer_message";
+}
